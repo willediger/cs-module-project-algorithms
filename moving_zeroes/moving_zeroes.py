627cb6db457b788320cedbb27752480137d5e070
Will Ediger <will.ediger@gmail.com>
2020-06-08 23:06:04 -0500

    moving zeroes in place solution. i think it's O(n) time complexity, O(1) space complexity

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,14 +2,25 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
 def moving_zeroes(arr):
     # Your code here
-
-    pass
+    zero_count = 0
+    i = 0
+    while i < len(arr) - zero_count:
+        if arr[i] == 0:
+            j = len(arr) - 1 - zero_count
+            while arr[j] == 0 and j > i:
+                j -= 1
+            arr[i] = arr[j]
+            arr[j] = 0
+            zero_count += 1
+        i += 1
+    return arr
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
-    arr = [0, 3, 1, 0, -2]
+    arr = [1, 2, 3, 0, 4, 0, 0]
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")

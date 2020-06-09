dcc882c0114d3d06e6c369155bf819c4f99d2265
Will Ediger <will.ediger@gmail.com>
2020-06-09 01:34:12 -0500

    naive and memoized solution for eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,29 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    if n <= 1:
-        return 1
-    elif n == 2:
-        return 2
-    else:
-        return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
+#naive solution
+# def eating_cookies(n):
+#     if n <= 1:
+#         return 1
+#     elif n == 2:
+#         return 2
+#     else:
+#         return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
+
+def eating_cookies(n, memo=[]):
+    memo.extend([0]*(n-len(memo)+1))
+    def eating_cookies_memoized(n):
+        if memo[n]:
+            return memo[n]
+        if n <= 1:
+            memo[n] = 1
+        elif n == 2:
+            memo[n] = 2
+        else:
+            memo[n] = eating_cookies_memoized(n-3) + eating_cookies_memoized(n-2) + eating_cookies_memoized(n-1)
+        return memo[n]
+    eating_cookies_memoized(n)
+    return memo[n]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation

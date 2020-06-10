8ea98a9500cdf910fdff3d1c00dc19f07a511f7e
Will Ediger <will.ediger@gmail.com>
2020-06-09 21:29:53 -0500

    improved zero counting by including zeroes in the end of the array we didnt swap to there

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -7,11 +7,14 @@ def moving_zeroes(arr):
     # Your code here
     zero_count = 0
     i = 0
+    # go thru array. if we find a zero, we switch it with the first non-zero value we find
+    # from the end of the array, keeping track of how many zeroes we have encountered.
     while i < len(arr) - zero_count:
         if arr[i] == 0:
             j = len(arr) - 1 - zero_count
             while arr[j] == 0 and j > i:
                 j -= 1
+                zero_count += 1
             arr[i] = arr[j]
             arr[j] = 0
             zero_count += 1
